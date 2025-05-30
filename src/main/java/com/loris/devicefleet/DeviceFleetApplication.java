c8cef91179e6bfff827488e89c1a0f0c84f8e217
Victor Carvalho <victorldcarvalho@gmail.com>
2025-05-29 21:23:25 -0300

    init Device Fleet project

---
+++ src/main/java/com/loris/devicefleet/DeviceFleetApplication.java
@@ -0,0 +1,13 @@
+package com.loris.devicefleet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceFleetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceFleetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loris/devicefleet/DeviceFleetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loris.devicefleet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceFleetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
